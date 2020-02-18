8b7f7b4886236581b010e223b7935ec4111dde34
= <=>
2020-02-18 15:48:50 +0700

    co ban xong file 1.js

---
+++ 1.js
@@ -0,0 +1,23 @@
+﻿ document.addEventListener("DOMContentLoaded",function(){
+ 	var myCenter = new google.maps.LatLng(10.7738297, 106.7036873);
+
+function initialize() {
+var mapProp = {
+  center:myCenter,
+  zoom:12,
+  scrollwheel:false,
+  draggable:false,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker = new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+ },false)
